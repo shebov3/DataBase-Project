c2f3499df8de917f5842b73d688a5a3ddd790466
C1earVision <133123242+C1earVision@users.noreply.github.com>
2024-12-07 02:58:07 +0200

    update
    update

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -9,9 +9,27 @@ const app = express();
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
 //extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
+
+app.use(express.json());
+app.set('trust proxy', 1);
+app.use(express.json());
+app.use(helmet())
+app.use(xss())
+app.use(cors({
+  origin: ["https://book-app-store.netlify.app","http://localhost:5173"]
+}))
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, //limit each ip to 100 requests per windowMs
+}))
+
 
 //routes
-app.use(express.json());
 app.use('/api/v1', noAuthRoutes)
 app.use('/api/v1/auth', auth)
 app.use('/api/v1/user', routeAuth, reqAuthRoutes)
--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -5,61 +5,55 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import imageSrc from "../images/images.jpeg";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 const Home = ({ setGameId, gameId }) => {
-  // dummy data
-  const cardData = [
-    {
-      image: imageSrc,
-      title: "Action Game",
-      price: "1699",
-      description: "some description",
-      onClick: () => setGameId("action-game-1"),
-    },
-    {
-      image: imageSrc,
-      title: "Action Game",
-      price: "1699",
-      description: "some description",
-      onClick: () => setGameId("action-game-2"),
-    },
-    {
-      image: imageSrc,
-      title: "Action Game",
-      price: "1699",
-      description: "some description",
-      onClick: () => setGameId("action-game-3"),
-    },
-    {
-      image: imageSrc,
-      title: "Action Game",
-      price: "1699",
-      description: "some description",
-      onClick: () => setGameId("action-game-4"),
-    },
-    {
-      image: imageSrc,
-      title: "Action Game",
-      price: "1699",
-      description: "some description",
-      onClick: () => setGameId("action-game-4"),
-    },
-    {
-      image: imageSrc,
-      title: "Action Game",
-      price: "1699",
-      description: "some description",
-      onClick: () => setGameId("action-game-4"),
-    },
-    
-  ];
+  const [data, setData] = useState([]);
+  
+  
+  useEffect(() => {
+      const getData = async () => {
+        const data = await axios.get(
+          `http://localhost:3000/api/v1/products`
+        );
+        setData(data.data.games);
+      };
+      getData();  
+    }, []);
+
+    const settings = {
+      dots: true, // Show navigation dots
+      infinite: true, // Infinite looping
+      speed: 500, // Transition speed
+      slidesToShow: 1, // Number of slides to display
+      slidesToScroll: 1, // Number of slides to scroll
+      autoplay: true, // Enable autoplay
+      autoplaySpeed: 3000, // Time between slide transitions in milliseconds
+    }
 
   return (
     <>
-    <Category title = "Action" items = {cardData}/>
-    <Category title = "Adventure" items = {cardData}/>
-    <Category title = "RPG" items = {cardData}/>
+      {/* <Category title = "Action" items = {cardData}/>
+      <Category title = "Adventure" items = {cardData}/>
+      <Category title = "RPG" items = {cardData}/> */}
+      <div className="flex justify-center mt-4">
+        <Slider className="w-[50rem]" {...settings}>
+          {data.map((game) => (
+            <div key={game.ProductId} className="">
+              <img
+                src={"https://via.placeholder.com/600x300"}
+                alt={game.Name}
+                className="w-full h-auto rounded-lg"
+              />
+              <h3 className="text-center text-lg font-medium mt-2">{game.Name}</h3>
+            </div>
+          ))}
+        </Slider>
+      </div>
     </>
+    
   );
 };
 
