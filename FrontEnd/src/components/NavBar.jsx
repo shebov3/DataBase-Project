059c9359c928e8154e6f1d42091a6db9054e5667
C1earVision <133123242+C1earVision@users.noreply.github.com>
2024-12-14 21:04:16 +0200

    udpate
    update

--- FrontEnd/src/components/NavBar.jsx
+++ FrontEnd/src/components/NavBar.jsx
@@ -52,13 +52,6 @@ const NavBar = ({ searchData, setSearchData, userData, setUserData }) => {
                 </button>
                 {dropdownOpen && (
                   <div className="absolute right-0 mt-2 w-40 bg-white rounded-lg shadow-lg">
-                    <button
-                      onClick={handleLogout}
-                      className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:text-red-500 transition duration-200 rounded-md"
-                    >
-                      <i className="fa-solid fa-right-from-bracket mr-2"></i> 
-                      <span className="ml-2">Log Out</span> 
-                    </button>
                     <Link 
                       to={'/account'}
                       className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:text-[rgb(149,171,82)] transition duration-200 rounded-md"
@@ -66,6 +59,14 @@ const NavBar = ({ searchData, setSearchData, userData, setUserData }) => {
                       <i className="fa-solid fa-file-invoice"></i>
                       <span className="ml-4">Account</span> 
                     </Link>
+                    
+                    <button
+                      onClick={handleLogout}
+                      className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:text-red-500 transition duration-200 rounded-md"
+                    >
+                      <i className="fa-solid fa-right-from-bracket mr-2"></i> 
+                      <span className="ml-2">Log Out</span> 
+                    </button>
                   </div>
                 )}
               </div>
--- FrontEnd/src/pages/Account.jsx
+++ FrontEnd/src/pages/Account.jsx
@@ -6,8 +6,54 @@ const Account = ({ searchData, setSearchData, userData, setUserData }) => {
 
 
   return (
-    <>Account</>
-  )
+      <div className="bg-gray-900 h-screen flex">
+        <div className="w-1/4 bg-gray-800 p-4">
+          <h2 className="text-white text-lg font-bold mb-4">Manage Your Account</h2>
+          <ul className="flex flex-col gap-2">
+            <Link to={'/account'} className="text-white text-sm mb-2 hover:text-blue-400 transition-colors duration-200">
+              My Account
+            </Link>
+            <Link className="text-white text-sm mb-2 hover:text-blue-400 transition-colors duration-200">
+              Orders
+            </Link>
+            <Link className="text-white text-sm mb-2 hover:text-blue-400 transition-colors duration-200">
+              Personal Information
+            </Link>
+            <Link className="text-white text-sm mb-2 hover:text-blue-400 transition-colors duration-200">
+              Shipping Information
+            </Link>
+            <Link className="text-white text-sm mb-2 hover:text-blue-400 transition-colors duration-200">
+              Reward Points
+            </Link>
+          </ul>
+        </div>
+
+        <div className="w-3/4 p-8">
+          <h2 className="text-white text-lg font-bold mb-4">Welcome Ahmed Ebrahim</h2>
+          <p className="text-white text-sm mb-4">What would you like to do?</p>
+
+          <div className="grid grid-cols-2 gap-4">
+            <div className="bg-gray-800 p-4 cursor-pointer rounded-lg transform transition-all duration-300 hover:bg-gray-700">
+              <h3 className="text-white text-md font-bold mb-2">Orders</h3>
+              <p className="text-white text-sm">Manage your orders.</p>
+            </div>
+            <div className="bg-gray-800 p-4 cursor-pointer rounded-lg transform transition-all duration-300 hover:bg-gray-700">
+              <h3 className="text-white text-md font-bold mb-2">Personal Information</h3>
+              <p className="text-white text-sm">Edit your personal information.</p>
+            </div>
+            <div className="bg-gray-800 p-4 cursor-pointer rounded-lg transform transition-all duration-300 hover:bg-gray-700">
+              <h3 className="text-white text-md font-bold mb-2">Shipping</h3>
+              <p className="text-white text-sm">Edit your delivery addresses.</p>
+            </div>
+            <div className="bg-gray-800 p-4 cursor-pointer rounded-lg transform transition-all duration-300 hover:bg-gray-700">
+              <h3 className="text-white text-md font-bold mb-2">Cart</h3>
+              <p className="text-white text-sm">Manage your Cart.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+  );
 }
 
 export default Account;
